f35e925e967745076a30958b817bf1179f085f69
Nic Desilets <nic@desilets.org>
2016-12-16 18:01:25 -0800

    Added example graph

--- nic/test/app/components/results/results.js
+++ nic/test/app/components/results/results.js
@@ -1,11 +1,11 @@
-(function(){
+(function () {
     'use strict';
-    angular.module('toolkit').directive('results', function($state, queryService){
-        return{
+    angular.module('toolkit').directive('results', function ($state, queryService) {
+        return {
             restrict: 'E',
             templateUrl: 'app/components/results/results.html',
             scope: {},
-            link: function(scope){
+            link: function (scope) {
                 /* --- On page load --- */
 
                 // Init vars
@@ -15,13 +15,52 @@
                 scope.tables = {};
                 scope.options = {};
 
-                queryService.runQuery().then(function(results){
+                // Dummy graph
+                var ctx = document.getElementById("myChart");
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                        datasets: [{
+                            label: '# of Votes',
+                            data: [12, 19, 3, 5, 2, 3],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+                });
+
+                queryService.runQuery().then(function (results) {
                     console.log(results);
                     scope.results = results;
 
                     scope.tables = parseTables(results);
                     scope.loading = false;
-                }, function(e){
+                }, function (e) {
                     console.error(e);
                     scope.error = true;
                     scope.loading = false;
@@ -31,26 +70,26 @@
 
                 // Format table results as an object rather than an array
                 // [{id: 'q', data: {...}}] -> {q: {headers: [], rows: []}}
-                function parseTables(results){
+                function parseTables(results) {
                     var parsed = {};
 
                     // For each table received
-                    for(var i in results){
+                    for (var i in results) {
                         var table = results[i];
                         var tableName = table.id;
                         var tableData = table.data;
 
                         // Setup table header
-                        var headers = []; 
+                        var headers = [];
                         var metadata = tableData.metaData;
-                        for(var key in metadata){
+                        for (var key in metadata) {
                             var colname = metadata[key].name;
                             headers.push(colname);
                         }
 
                         // Setup table rows 
                         var rows = [];
-                        for(var i in tableData.rows){
+                        for (var i in tableData.rows) {
                             var row = tableData.rows[i];
                             rows.push(row);
                         }
@@ -60,7 +99,7 @@
                             rows: rows
                         };
 
-                        switch(tableName){
+                        switch (tableName) {
                             case 'pei':
                                 scope.options.pei = true;
                                 break;
