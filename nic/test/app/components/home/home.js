17a10dcddf1f0bbb960c69e1169fd0450c3e035b
Nic Desilets <nic@desilets.org>
2016-12-20 18:47:30 -0800

    Changed default query, fixed PEI script (forgot wildcards)

--- nic/test/app/components/home/home.js
+++ nic/test/app/components/home/home.js
@@ -9,7 +9,7 @@
                 /* --- On page load --- */
 
                 // Init vars 
-                scope.query = 'SELECT * FROM CAPSTONE_PARALLEL_TEST_V1;';
+                scope.query = 'SELECT * FROM CAPSTONE_PARALLEL_TEST_V1 ORDER BY PRESS_LOCAL_TIME DESC;';
                 scope.trace = '';
                 scope.options = {};
 
