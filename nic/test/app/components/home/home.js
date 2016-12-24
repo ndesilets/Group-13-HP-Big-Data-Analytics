0069c2bc174f15b41841aa5bdbbb28a62d1aeec9
Whitlock <nathaniel.whitlock1@hp.com>
2016-12-23 17:18:33 -0800

    Non-functional changes in trying to implement trace feature

--- nic/test/app/components/home/home.js
+++ nic/test/app/components/home/home.js
@@ -16,8 +16,25 @@
                 /* --- User actions --- */
 
                 scope.submit = function(){
-                    queryService.setQuery(scope.query, scope.trace, scope.options);
-                    $state.go('Results');
+                 if(scope.trace !== ''){
+                        console.log("Trace query start"); 
+
+                        // Consider how we can ensure uniqueness for tracefile_identifier 
+                        scope.traceStart = "alter session set tracefile_identifier='TEST-" + scope.trace + "; alter session set timed_statistics = true; alter session set statistics_level=all; alter session set max_dump_file_size = unlimited; alter session set events '" + scope.trace + " trace name context forever,level 12';";
+
+                        scope.traceEnd = "alter session set events '" + scope.trace + " trace name context off';";
+
+                        for(var key in scope){
+                            console.log('== key', key);
+                            console.log('== value', scope[key]);
+                        }
+                        queryService.setQuery(scope.traceStart, scope.query, scope.traceEnd, scope.options);
+                        $state.go('Results');
+                     }
+                     else{
+                        queryService.setQuery(scope.query, scope.trace, scope.options);
+                        $state.go('Results');
+                     }
                 }
             }
         };
