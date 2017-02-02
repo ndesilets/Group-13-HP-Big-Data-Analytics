98c8f16fe969975caabddb8a8bf69c07bc66fb3f
Kirby Sand <kirby.sand@hp.com>
2017-02-01 19:24:26 -0800

    Lots of stuff

---
+++ Kirby/Views/PARAMETERS_V.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE VIEW CAPSTONE_DEMO.PARAMETERS_V AS
+
+SELECT
+  A.NAME,
+  A.VALUE,
+  A.DISPLAY_VALUE,
+  A.ISDEFAULT,
+  A.ISSES_MODIFIABLE,
+  A.ISSYS_MODIFIABLE,
+  A.ISPDB_MODIFIABLE,
+  A.ISINSTANCE_MODIFIABLE,
+  A.ISMODIFIED,
+  A.ISADJUSTED,
+  A.DESCRIPTION
+FROM V$PARAMETER A
+WHERE NAME IN (
+                'pga_aggregate_limit', 
+                'pga_aggregate_target', 
+                '_pga_max_size', 
+                'sga_max_size', 
+                'sga_target', 
+                'db_cache_size', 
+                'db_big_table_cache_percent_target', 
+                'parallel_degree_policy', 
+                'parallel_min_time_threshold',
+                'inmemory_size',
+                'parallel_servers_target',
+                'parallel_degree_limit',
+                'parallel_degree_level',
+                'parallel_threads_per_cpu',
+                'parallel_execution_message_size',
+                'parallel_min_servers',
+                'parallel_max_servers',
+                'parallel_adaptive_multi_user'
+               )
+ORDER BY
+  NAME;
+  
+SELECT
+  *
+FROM CAPSTONE_DEMO.PARAMETERS_V;
