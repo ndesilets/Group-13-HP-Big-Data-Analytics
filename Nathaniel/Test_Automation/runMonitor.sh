984c85afd2ea7863fc7334fde7837aa9aef62527
Whitlock <nathaniel.whitlock1@hp.com>
2017-02-04 23:35:28 -0800

    Test automation suite in working state

---
+++ Nathaniel/Test_Automation/runMonitor.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# This script is designed to create connection to Oracle
+# database and run the resource usage monitor in the
+# background
+
+# Store connect string
+DBUSER='whitlocn'
+DBUSERPASSWORD='capPass'
+DB='dbcap'
+
+# Store passed arguments
+logDir=${1}
+dataDir=${2}
+snapFreq=${3}
+
+# Establish connection and run monitoring loop
+sqlplus /nolog  <<EOF  > ${logDir}/query.log
+CONN ${DBUSER}/${DBUSERPASSWORD}@${DB}
+
+spool ${dataDir}/runMonitor_output.dat
+EXEC CAPSTONE_DEMO.RESOURCE_MONITOR.RESOURCE_USAGE_MONITORING_V2(2000,${snapFreq});
+spool off
+
+EOF
+
+echo 'Ending Monitoring Loop'
