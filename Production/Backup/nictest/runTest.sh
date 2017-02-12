e7cf4b95cb4836598fa5fd558d85827b1a51914f
natewhit44 <natewhit44@gmail.com>
2017-02-12 10:03:22 -0800

    Test-Nate

---
+++ Production/Backup/nictest/runTest.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This script is designed to facilitate the running of
+# tests which will be monitored, the resulting data
+# can be used to validate parameter setting effects
+
+# Store passed arguments
+logDir=$1
+dataDir=$2
+expScript=$3
+dbUser=$4
+password=$5
+db=$6
+
+#echo -e '\n\n--------------------------'
+#echo 'runTest.sh Input Vars'
+#echo 'logDir: ' $logDir
+#echo 'dataDir: ' $dataDir
+#echo 'dbuser: ' $dbUser
+#echo 'password: ' $password
+#echo 'db: ' $db
+#echo -e '--------------------------\n\n'
+
+# Establish connection and execute predefined queries
+sqlplus /nolog <<EOF >> ${logDir}/runTest.log 
+CONN ${dbUser}/${password}@${db}
+
+@${expScript}
+
+@../exp_scripts/END_MONITORING.sql
+
+EOF
+
+
+echo 'Finished Testing Suite -- ' ${expScript}
+
