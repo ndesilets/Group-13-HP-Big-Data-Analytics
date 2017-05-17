1ce87ab07981227b63e3e02dd115fce1e7006461
Nic Desilets <nic@desilets.org>
2017-05-17 15:29:03 -0700

    Added back button if not at home

--- Not_For_Grading/visualization/src/app/app.component.ts
+++ Not_For_Grading/visualization/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(){
-    
+  private homeUrl: string = "/experiments";
+  private isHome: boolean = true;
+
+  constructor(private router: Router){
+    router.events.subscribe((ev) => {
+      if(ev instanceof NavigationEnd){
+        let url = ev.url || null;
+
+        if(url && url != this.homeUrl){
+          this.isHome = false;
+        }else{
+          this.isHome = true;
+        }
+      }
+    });
+  }
+
+  private goBack(){
+    window.history.back();
   }
 }
